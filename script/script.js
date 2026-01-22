afc0f146626745ed64662a0a4f23e1410c09f41f
deepasivasami <sivasami483@gmail.com>
2026-01-22 15:07:09 +0530

    first commit

---
+++ script/script.js
@@ -0,0 +1,23 @@
+
+
+
+
+// out put
+
+
+alert('hi ia am nilla')
+document.writeln("he i am deepa")
+window.alert("what is you name?")
+console.log("my name is selv")
+window.print("print")
+
+
+
+// input 
+
+
+
+
+
+prompt("enter you name?")
+confirm("do you want to logout")
